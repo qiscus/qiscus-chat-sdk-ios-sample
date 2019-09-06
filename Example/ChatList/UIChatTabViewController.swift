6dafcd9b39b346984964f29e46f82a9484c3a07b
ariefnurputranto <arief@qiscus.co>
2019-09-06 17:34:56 +0700

    revamp UI getCustomer

--- Example/ChatList/UIChatTabViewController.swift
+++ Example/ChatList/UIChatTabViewController.swift
@@ -38,81 +38,100 @@ class UIChatTabViewController: ButtonBarPagerTabStripViewController {
         super.viewDidLoad()
         self.setupUI()
         self.setupUINavBar()
-        self.setupAdminOrAgent()
-    }
-    func setupAdminOrAgent(){
-        if let userType = UserDefaults.standard.getUserType(){
-            if userType == 2 {
-                getCountCustomer()
-                
-                if (self.timer != nil) {
-                    self.timer?.invalidate()
-                    self.timer = nil
-                }
-                self.timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(getCountCustomer), userInfo: nil, repeats: true)
-            }
+        self.getCountCustomer()
+        
+        if (self.timer != nil) {
+            self.timer?.invalidate()
+            self.timer = nil
         }
+        self.timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(getCountCustomer), userInfo: nil, repeats: true)
     }
+    
     
     @objc func getCountCustomer(){
         guard let token = UserDefaults.standard.getAuthenticationToken() else {
             return
         }
-   
+        
         let header = ["Authorization": token] as [String : String]
         
         Alamofire.request("https://qismo.qiscus.com/api/v1/admin/service/get_unresolved_count", method: .get, parameters: nil, headers: header as! HTTPHeaders).responseJSON { (response) in
             if response.result.value != nil {
                 if (response.response?.statusCode)! >= 300 {
-                    self.configureExpandingMenuButton()
+                   self.createFloatingButton(count: 0)
                 } else {
                     //success
                     let payload = JSON(response.result.value)
                     let count = payload["data"]["total_unresolved"].int ?? 0
-                    self.configureExpandingMenuButton(value: count)
+                    
+                    self.createFloatingButton(count: count)
                     
                 }
             } else if (response.response != nil && (response.response?.statusCode)! == 401) {
                 //failed
-                self.configureExpandingMenuButton()
+                self.createFloatingButton(count: 0)
             } else {
                 //failed
-                self.configureExpandingMenuButton()
+               self.createFloatingButton(count: 0)
             }
         }
     }
     
-    func getCustomer(){
-        guard let token = UserDefaults.standard.getAuthenticationToken() else {
-            return
+    func createFloatingButton(count: Int){
+        if let viewWithTag = self.view.viewWithTag(222) {
+            viewWithTag.removeFromSuperview()
         }
         
-        let header = ["Authorization": token] as [String : String]
+        let button = UIButton()
         
-        Alamofire.request("https://qismo.qiscus.com/api/v1/agent/service/takeover_unresolved_room", method: .post, parameters: nil, headers: header as! HTTPHeaders).responseJSON { (response) in
-            if response.result.value != nil {
-                if (response.response?.statusCode)! >= 300 {
-                   //failed
-                    let payload = JSON(response.result.value)
-                    let errors = payload["errors"].string ?? "Failed Get Customer"
-                    self.showAlert(errors)
-                } else {
-                    //success
-                    let payload = JSON(response.result.value)
-                    print("check payload2 =\(payload)")
-                    self.getCountCustomer()
-                    NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "reloadListRoom"), object: nil)
-                    self.showAlert("Success Get Customer")
-                    
-                }
-            } else if (response.response != nil && (response.response?.statusCode)! == 401) {
-                //failed
-                self.showAlert("Failed Get Customer")
-            } else {
-                //failed
-                self.showAlert("Failed Get Customer")
+        if UIDevice().userInterfaceIdiom == .phone {
+            switch UIScreen.main.nativeBounds.height {
+            case 1920, 2208:
+                button.frame = CGRect(x: self.view.frame.size.width - 105 , y: self.view.frame.size.height - 105, width: 100, height: 100)
+            default:
+                button.frame = CGRect(x: self.view.frame.size.width - 80 , y: self.view.frame.size.height - 80, width: 75, height: 75)
             }
         }
+        
+        if let userType = UserDefaults.standard.getUserType(){
+            if userType == 2 {
+                if (count == 0){
+                    button.setImage(UIImage(named: "ic_agent_floating_no"), for: .normal)
+                }else{
+                    button.setImage(UIImage(named: "ic_agent_floating"), for: .normal)
+                }
+            }else{
+                if (count == 0){
+                    button.setImage(UIImage(named: "ic_admin_floating_no"), for: .normal)
+                }else{
+                    button.setImage(UIImage(named: "ic_admin_floating"), for: .normal)
+                }
+                
+            }
+        }else{
+            button.setImage(UIImage(named: "ic_agent_floating_no"), for: .normal)
+        }
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.layer.shadowColor = UIColor(red: 0.35, green: 0.44, blue: 0.25, alpha: 0.25).cgColor
+        button.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        button.layer.shadowOpacity = 1.0
+        button.layer.shadowRadius = 4
+        self.view.addSubview(button)
+
+        button.tag = 222
+        self.view.addSubview(button)
+        self.view.viewWithTag(222)?.bringSubviewToFront(self.view)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        let popupVC = UIBottomPopupVC()
+        popupVC.width = self.view.frame.size.width
+        popupVC.topCornerRadius = 15
+        popupVC.presentDuration = 0.30
+        popupVC.dismissDuration = 0.30
+        popupVC.shouldDismissInteractivelty = true
+        //popupVC.popupDelegate = self
+        self.present(popupVC, animated: true, completion: nil)
     }
     
     func showAlert(_ title: String) {
@@ -120,74 +139,6 @@ class UIChatTabViewController: ButtonBarPagerTabStripViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-    
-    fileprivate func configureExpandingMenuButton(value : Int = 0) {
-        
-        if let viewWithTag = self.view.viewWithTag(222) {
-            viewWithTag.removeFromSuperview()
-        }
-        
-        var menuButtonSize: CGSize = CGSize(width: 80.0, height: 80.0)
-        var menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "plus")!, rotatedImage: UIImage(named: "plus")!)
-        
-        if UIDevice().userInterfaceIdiom == .phone {
-            switch UIScreen.main.nativeBounds.height {
-            case 1136:
-                print("iPhone 5 or 5S or 5C")
-                menuButtonSize = CGSize(width: 80.0, height: 80.0)
-                menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "plus")!, rotatedImage: UIImage(named: "plus")!)
-                menuButton.center = CGPoint(x: (self.view.frame.size.width - 40.0), y: self.view.frame.size.height - 40.0)
-            case 1334:
-                print("iPhone 6/6S/7/8")
-                menuButtonSize = CGSize(width: 100.0, height: 100.0)
-                menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "plus")!, rotatedImage: UIImage(named: "plus")!)
-                menuButton.center = CGPoint(x: (self.view.frame.size.width - 50.0), y: self.view.frame.size.height - 50.0)
-            case 1920, 2208:
-                print("iPhone 6+/6S+/7+/8+")
-                menuButtonSize = CGSize(width: 120.0, height: 120.0)
-                menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "plus")!, rotatedImage: UIImage(named: "plus")!)
-                menuButton.center = CGPoint(x: (self.view.frame.size.width - 50.0), y: self.view.frame.size.height - 50.0)
-            case 2436:
-                print("iPhone X, XS")
-                menuButtonSize = CGSize(width: 100.0, height: 100.0)
-                menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "plus")!, rotatedImage: UIImage(named: "plus")!)
-                menuButton.center = CGPoint(x: (self.view.frame.size.width - 50.0), y: self.view.frame.size.height - 50.0)
-            case 2688:
-                print("iPhone XS Max")
-                menuButtonSize = CGSize(width: 100.0, height: 100.0)
-                menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "plus")!, rotatedImage: UIImage(named: "plus")!)
-                menuButton.center = CGPoint(x: (self.view.frame.size.width - 50.0), y: self.view.frame.size.height - 50.0)
-            case 1792:
-                print("iPhone XR")
-                menuButtonSize = CGSize(width: 100.0, height: 100.0)
-                menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "plus")!, rotatedImage: UIImage(named: "plus")!)
-                menuButton.center = CGPoint(x: (self.view.frame.size.width - 50.0), y: self.view.frame.size.height - 50.0)
-            default:
-                print("Unknown")
-                menuButtonSize = CGSize(width: 120.0, height: 120.0)
-                menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "plus")!, rotatedImage: UIImage(named: "plus")!)
-                menuButton.center = CGPoint(x: (self.view.frame.size.width - 50.0), y: self.view.frame.size.height - 50.0)
-            }
-        }
-        
-        menuButton.tag = 222
-        self.view.addSubview(menuButton)
-        
-        let item1 = ExpandingMenuItem(size: menuButtonSize, title: "Get Customer \(value)", image: UIImage(named: "ic_cs")!, highlightedImage: UIImage(named: "chooser-moment-icon-place-highlighted")!, backgroundImage: UIImage(named: "ic_circle"), backgroundHighlightedImage: UIImage(named: "chooser-moment-button-highlighted")) { () -> Void in
-            self.getCustomer()
-        }
-        
-        menuButton.addMenuItems([item1])
-        
-        menuButton.willPresentMenuItems = { (menu) -> Void in
-            print("MenuItems will present.")
-        }
-        
-        menuButton.didDismissMenuItems = { (menu) -> Void in
-            print("MenuItems dismissed.")
-        }
-    }
-    
     func setupUI(){
         settings.style.selectedBarHeight = 1
         
