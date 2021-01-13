713fb951c50b52b741a54fff16ecd93f44ad0d75
ariefnurputranto <arief@qiscus.co>
2021-01-13 09:29:15 +0700

    implement video cell

--- Example/ChatView/ChatCell/video/QVideoLeftCell.swift
+++ Example/ChatView/ChatCell/video/QVideoLeftCell.swift
@@ -7,18 +7,206 @@
 //
 
 import UIKit
+import QiscusCore
+import AlamofireImage
+import Alamofire
+import SimpleImageViewer
+import SDWebImage
+import SDWebImageWebPCoder
 
-class QVideoLeftCell: UITableViewCell {
-
+class QVideoLeftCell: UIBaseChatCell {
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var tvContent: UILabel!
+    @IBOutlet weak var ivBaloonLeft: UIImageView!
+    @IBOutlet weak var lbTime: UILabel!
+    @IBOutlet weak var viewContainer: UIView!
+    @IBOutlet weak var ivStatus: UIImageView!
+    @IBOutlet weak var ivComment: UIImageView!
+    @IBOutlet weak var ivPlay: UIImageView!
+    @IBOutlet weak var lbNameHeight: NSLayoutConstraint!
+    @IBOutlet weak var lbNameLeading: NSLayoutConstraint!
+    @IBOutlet weak var rightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leftConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var ivAvatarUser: UIImageView!
+    var isPublic: Bool = false
+    var menuConfig = enableMenuConfig()
+    var colorName : UIColor = UIColor.black
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.setMenu()
+        self.ivComment.contentMode = .scaleAspectFill
+        self.ivComment.clipsToBounds = true
+         self.ivComment.layer.cornerRadius = 8
+        self.ivComment.backgroundColor = UIColor.black
+        self.ivComment.isUserInteractionEnabled = true
+        let imgTouchEvent = UITapGestureRecognizer(target: self, action: #selector(QVideoLeftCell.playDidTap))
+        self.ivComment.addGestureRecognizer(imgTouchEvent)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        self.setMenu()
         // Configure the view for the selected state
+    }
+    
+    override func present(message: CommentModel) {
+        self.bindData(message: message)
+    }
+    
+    override func update(message: CommentModel) {
+        self.bindData(message: message)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        ivComment.image = nil // or place holder image
+    }
+    
+    func bindData(message: CommentModel){
+        self.setupBalon()
+        
+        // get image
+        self.lbTime.text = self.hour(date: message.date())
+        self.lbTime.textColor = ColorConfiguration.timeLabelTextColor
+        guard let payload = message.payload else { return }
+        let caption = payload["caption"] as? String
+        
+        self.tvContent.text = caption
+        self.tvContent.textColor = ColorConfiguration.leftBaloonTextColor
+        
+        if let url = payload["url"] as? String {
+            var fileImage = url
+            if fileImage.isEmpty {
+                fileImage = "https://"
+            }
+            
+            self.ivComment.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+            
+            QiscusCore.shared.getThumbnailURL(url: fileImage) { (thumbURL) in
+                self.ivComment.sd_imageIndicator = SDWebImageActivityIndicator.grayLarge
+                self.ivComment.sd_setImage(with: URL(string: thumbURL) ?? URL(string: "https://"), placeholderImage: nil, options: .highPriority) { (uiImage, error, cache, urlPath) in
+                    if urlPath != nil && uiImage != nil{
+                        self.ivComment.af_setImage(withURL: urlPath!)
+                    }
+                }
+            } onError: { (error) in
+                self.ivComment.sd_imageIndicator = SDWebImageActivityIndicator.grayLarge
+                self.ivComment.sd_setImage(with: URL(string: url) ?? URL(string: "https://"), placeholderImage: nil, options: .highPriority) { (uiImage, error, cache, urlPath) in
+                    if urlPath != nil && uiImage != nil{
+                        self.ivComment.af_setImage(withURL: urlPath!)
+                    }
+                }
+            }
+        }else{
+            var fileImage = message.getAttachmentURL(message: message.message)
+            
+            if fileImage.isEmpty {
+                fileImage = "https://"
+            }
+            
+            QiscusCore.shared.getThumbnailURL(url: fileImage) { (thumbURL) in
+                self.ivComment.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+                self.ivComment.sd_imageIndicator = SDWebImageActivityIndicator.grayLarge
+                self.ivComment.sd_setImage(with: URL(string: thumbURL) ?? URL(string: "https://"), placeholderImage: nil, options: .highPriority) { (uiImage, error, cache, urlPath) in
+                    if urlPath != nil && uiImage != nil{
+                        self.ivComment.af_setImage(withURL: urlPath!)
+                    }
+                }
+            } onError: { (error) in
+                self.ivComment.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+                self.ivComment.sd_imageIndicator = SDWebImageActivityIndicator.grayLarge
+                self.ivComment.sd_setImage(with: URL(string: fileImage) ?? URL(string: "https://"), placeholderImage: nil, options: .highPriority) { (uiImage, error, cache, urlPath) in
+                    if urlPath != nil && uiImage != nil{
+                        self.ivComment.af_setImage(withURL: urlPath!)
+                    }
+                }
+            }
+        }
+        
+        self.ivAvatarUser.layer.cornerRadius = self.ivAvatarUser.frame.size.width / 2
+        self.ivAvatarUser.clipsToBounds = true
+        
+        if let avatar = message.userAvatarUrl {
+            if avatar.absoluteString.contains("https://image.flaticon.com/icons/svg/145/145867.svg") == true{
+                self.ivAvatarUser.af_setImage(withURL: URL(string:"https://d1edrlpyc25xu0.cloudfront.net/ziv-nqsjtf0zdqf6kfk7s/image/upload/w_320,h_320,c_limit/r7byw7m9e4/default-wa.png")!)
+            }else{
+                self.ivAvatarUser.af_setImage(withURL: message.userAvatarUrl ?? URL(string: "http://")!)
+            }
+        }else{
+            self.ivAvatarUser.af_setImage(withURL: message.userAvatarUrl ?? URL(string: "http://")!)
+        }
+        
+        if(isPublic == true){
+            self.lbName.text = message.username
+            self.lbName.textColor = colorName
+            lbNameHeight.constant = 21
+        }else{
+            self.lbName.text = ""
+            lbNameHeight.constant = 0
+        }
+        
+    }
+    
+   
+    
+    @objc func playDidTap() {
+        guard let payload = self.comment?.payload else { return }
+        if let fileName = payload["file_name"] as? String{
+            if let url = payload["url"] as? String {
+                let preview = ChatPreviewDocVC()
+                preview.fileName = fileName
+                preview.url = url
+                preview.roomName = "Video Preview"
+                let backButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+                
+                self.currentViewController()?.navigationItem.backBarButtonItem = backButton
+                self.currentViewController()?.navigationController?.pushViewController(preview, animated: true)
+            }
+        }
+       
+    }
+   
+    
+    func currentViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        
+        if let nav = base as? UINavigationController {
+            return currentViewController(base: nav.visibleViewController)
+        }
+        
+        if let tab = base as? UITabBarController {
+            let moreNavigationController = tab.moreNavigationController
+            
+            if let top = moreNavigationController.topViewController, top.view.window != nil {
+                return currentViewController(base: top)
+            } else if let selected = tab.selectedViewController {
+                return currentViewController(base: selected)
+            }
+        }
+        
+        if let presented = base?.presentedViewController {
+            return currentViewController(base: presented)
+        }
+        
+        return base
+    }
+    
+    func setupBalon(){
+        self.ivBaloonLeft.applyShadow()
+        self.ivBaloonLeft.image = self.getBallon()
+        self.ivBaloonLeft.tintColor = ColorConfiguration.leftBaloonColor
+    }
+    
+    func hour(date: Date?) -> String {
+        guard let date = date else {
+            return "-"
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        formatter.timeZone      = TimeZone.current
+        let defaultTimeZoneStr = formatter.string(from: date);
+        return defaultTimeZoneStr
     }
     
 }
--- Example/ChatView/ChatCell/video/QVideoRightCell.swift
+++ Example/ChatView/ChatCell/video/QVideoRightCell.swift
@@ -7,18 +7,217 @@
 //
 
 import UIKit
+import QiscusCore
 
-class QVideoRightCell: UITableViewCell {
+import AlamofireImage
+import Alamofire
+import SimpleImageViewer
+import SDWebImageWebPCoder
 
+class QVideoRightCell: UIBaseChatCell {
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var tvContent: UILabel!
+    @IBOutlet weak var ivBaloonLeft: UIImageView!
+    @IBOutlet weak var lbTime: UILabel!
+    @IBOutlet weak var viewContainer: UIView!
+    @IBOutlet weak var ivStatus: UIImageView!
+    @IBOutlet weak var ivComment: UIImageView!
+    
+    @IBOutlet weak var lbNameHeight: NSLayoutConstraint!
+    @IBOutlet weak var lbNameTrailing: NSLayoutConstraint!
+    @IBOutlet weak var rightConstraint: NSLayoutConstraint!
+    var menuConfig = enableMenuConfig()
+    @IBOutlet weak var ivPlay: UIImageView!
+    var isQiscus : Bool = false
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.setMenu(isQiscus: isQiscus)
+        self.ivComment.contentMode = .scaleAspectFill
+        self.ivComment.clipsToBounds = true
+        self.ivComment.backgroundColor = UIColor.black
+        self.ivComment.layer.cornerRadius = 8
+        self.ivComment.isUserInteractionEnabled = true
+        let imgTouchEvent = UITapGestureRecognizer(target: self, action: #selector(QVideoRightCell.playDidTap))
+        self.ivComment.addGestureRecognizer(imgTouchEvent)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        self.setMenu(isQiscus: isQiscus)
+    }
+    
+    override func present(message: CommentModel) {
+        self.bindData(message: message)
+    }
+    
+    override func update(message: CommentModel) {
+        self.bindData(message: message)
+    }
+    
+    override func prepareForReuse() {
+      super.prepareForReuse()
+      ivComment.image = nil // or place holder image
+    }
+    
+    func bindData(message: CommentModel){
+        self.setupBalon()
+        self.status(message: message)
+        // get image
+        self.lbName.text = "You"
+        self.lbTime.text = self.hour(date: message.date())
+        guard let payload = message.payload else { return }
+        let caption = payload["caption"] as? String
+        
+        self.tvContent.text = caption
+        
+        self.tvContent.textColor = ColorConfiguration.rightBaloonTextColor
+        
+        if let url = payload["url"] as? String {
+            var fileImage = url
+            if fileImage.isEmpty {
+                fileImage = "https://"
+            }
+            
+            self.ivComment.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+            
+            QiscusCore.shared.getThumbnailURL(url: fileImage) { (thumbURL) in
+                self.ivComment.sd_imageIndicator = SDWebImageActivityIndicator.grayLarge
+                self.ivComment.sd_setImage(with: URL(string: thumbURL) ?? URL(string: "https://"), placeholderImage: nil, options: .highPriority) { (uiImage, error, cache, urlPath) in
+                    if urlPath != nil && uiImage != nil{
+                        self.ivComment.af_setImage(withURL: urlPath!)
+                    }
+                }
+            } onError: { (error) in
+                self.ivComment.sd_imageIndicator = SDWebImageActivityIndicator.grayLarge
+                self.ivComment.sd_setImage(with: URL(string: url) ?? URL(string: "https://"), placeholderImage: nil, options: .highPriority) { (uiImage, error, cache, urlPath) in
+                    if urlPath != nil && uiImage != nil{
+                        self.ivComment.af_setImage(withURL: urlPath!)
+                    }
+                }
+            }
+        }else{
+            var fileImage = message.getAttachmentURL(message: message.message)
+            
+            if fileImage.isEmpty {
+                fileImage = "https://"
+            }
+            
+            QiscusCore.shared.getThumbnailURL(url: fileImage) { (thumbURL) in
+                self.ivComment.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+                self.ivComment.sd_imageIndicator = SDWebImageActivityIndicator.grayLarge
+                self.ivComment.sd_setImage(with: URL(string: thumbURL) ?? URL(string: "https://"), placeholderImage: nil, options: .highPriority) { (uiImage, error, cache, urlPath) in
+                    if urlPath != nil && uiImage != nil{
+                        self.ivComment.af_setImage(withURL: urlPath!)
+                    }
+                }
+            } onError: { (error) in
+                self.ivComment.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+                self.ivComment.sd_imageIndicator = SDWebImageActivityIndicator.grayLarge
+                self.ivComment.sd_setImage(with: URL(string: fileImage) ?? URL(string: "https://"), placeholderImage: nil, options: .highPriority) { (uiImage, error, cache, urlPath) in
+                    if urlPath != nil && uiImage != nil{
+                        self.ivComment.af_setImage(withURL: urlPath!)
+                    }
+                }
+            }
+        }
+    }
+    
+    @objc func playDidTap() {
+        
+        guard let payload = self.comment?.payload else { return }
+        if let fileName = payload["file_name"] as? String{
+            if let url = payload["url"] as? String {
+                let preview = ChatPreviewDocVC()
+                preview.fileName = fileName
+                preview.url = url
+                preview.roomName = "Video Preview"
+                let backButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+                
+                self.currentViewController()?.navigationItem.backBarButtonItem = backButton
+                self.currentViewController()?.navigationController?.pushViewController(preview, animated: true)
+            }
+        }
+    }
+    
+    func currentViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        
+        if let nav = base as? UINavigationController {
+            return currentViewController(base: nav.visibleViewController)
+        }
+        
+        if let tab = base as? UITabBarController {
+            let moreNavigationController = tab.moreNavigationController
+            
+            if let top = moreNavigationController.topViewController, top.view.window != nil {
+                return currentViewController(base: top)
+            } else if let selected = tab.selectedViewController {
+                return currentViewController(base: selected)
+            }
+        }
+        
+        if let presented = base?.presentedViewController {
+            return currentViewController(base: presented)
+        }
+        
+        return base
+    }
+    
+    func setupBalon(){
+        //self.ivBaloonLeft.applyShadow()
+        self.ivBaloonLeft.image = self.getBallon()
+        self.ivBaloonLeft.tintColor = ColorConfiguration.rightBaloonColor
+    }
+    
+    func status(message: CommentModel){
+        
+        switch message.status {
+        case .deleted:
+            ivStatus.image = UIImage(named: "ic_deleted")?.withRenderingMode(.alwaysTemplate)
+            break
+        case .sending, .pending:
+            lbTime.textColor = ColorConfiguration.timeLabelTextColor
+            ivStatus.tintColor = ColorConfiguration.sentOrDeliveredColor
+            lbTime.text = TextConfiguration.sharedInstance.sendingText
+            ivStatus.image = UIImage(named: "ic_info_time")?.withRenderingMode(.alwaysTemplate)
+            break
+        case .sent:
+            lbTime.textColor = ColorConfiguration.timeLabelTextColor
+            ivStatus.tintColor = ColorConfiguration.sentOrDeliveredColor
+            ivStatus.image = UIImage(named: "ic_sending")?.withRenderingMode(.alwaysTemplate)
+            break
+        case .delivered:
+            lbTime.textColor = ColorConfiguration.timeLabelTextColor
+            ivStatus.tintColor = ColorConfiguration.readMessageColor
+            ivStatus.image = UIImage(named: "ic_read")?.withRenderingMode(.alwaysTemplate)
+            break
+        case .read:
+            lbTime.textColor = ColorConfiguration.timeLabelTextColor
+            ivStatus.tintColor = ColorConfiguration.readMessageColor
+            ivStatus.image = UIImage(named: "ic_read")?.withRenderingMode(.alwaysTemplate)
+            break
+        case . failed:
+            lbTime.textColor = ColorConfiguration.timeLabelTextColor
+            lbTime.text = TextConfiguration.sharedInstance.failedText
+            ivStatus.image = UIImage(named: "ic_warning")?.withRenderingMode(.alwaysTemplate)
+            ivStatus.tintColor = ColorConfiguration.failToSendColor
+            break
+        case .deleting:
+            ivStatus.image = UIImage(named: "ic_deleted")?.withRenderingMode(.alwaysTemplate)
+            break
+        }
+    }
+    
+    func hour(date: Date?) -> String {
+        guard let date = date else {
+            return "-"
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        formatter.timeZone      = TimeZone.current
+        let defaultTimeZoneStr = formatter.string(from: date);
+        return defaultTimeZoneStr
     }
     
 }
