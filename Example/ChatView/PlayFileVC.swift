0092802cfcc7747079b68ca082cdad0f03e13e7a
Arief Nur Putranto <arief@qiscus.co>
2022-02-16 13:03:24 +0700

    Bugfix, enhancement of new WhatsApp pricing and hide show start chat configuration

---
+++ Example/ChatView/PlayFileVC.swift
@@ -0,0 +1,30 @@
+//
+//  PlayFileVC.swift
+//  Example
+//
+//  Created by arief nur putranto on 04/02/22.
+//  Copyright © 2022 Qiscus. All rights reserved.
+//
+
+import UIKit
+
+class PlayFileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Example/Extension/AVAudioFileConverter.swift
@@ -0,0 +1,9 @@
+//
+//  AVAudioFileConverter.swift
+//  Example
+//
+//  Created by arief nur putranto on 03/02/22.
+//  Copyright © 2022 Qiscus. All rights reserved.
+//
+
+import Foundation
